4bf908bfb8ffefc9d69d63fdde393275b5781b13
vicha3663 <f.tramites@gmail.com>
2020-12-14 15:30:13 -0300

    hasta modal

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -3,24 +3,62 @@
         <div class="row">
           <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4  my-3" v-for="(item,index) in enviarDatosApi" :key="index">
              <div class="card">
-          <img :src="item.image" class="card-img-top" alt="item.image">
-      <div class="card-body">
-          <h5 class="card-title">{{item.name}}</h5>
-          <p class="card-text">p the bulk of the card's content.</p>
+                 <img :src="item.image" class="card-img-top" alt="item.image">
+              <div class="card-body">
+                  <h5 class="card-title">{{item.name}}</h5>
+               </div>
+                <div class="card-body">
+                  <a href="#" class="card-link" >Opiniones</a>
+                  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Ver más</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">{{item.name}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-          </ul>
-       <div class="card-body">
-           <a href="#" class="card-link">Opiniones</a>
-             <a href="#" class="card-link">Ver más</a>
+      <div class="modal-body">
+          <p> El estado del pesronaje es {{item.status}}</p>
+          <p> Vive en {{item.species}}</p>
+          <p> El personaje vive en {{item.origin.name}}</p>
+          <p> Ha participado en {{item.episode.length}} capítulos</p>
+          <p> Nació el {{item.created}}</p>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Agregar a Favoritos</button>
       </div>
     </div>
   </div>
-  </div>
 </div>
+        
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                </div>
+            </div>          
+           </div>
+        </div>
+    </div>
+
 </template>
 <script>
 // @ is an alias to /src
